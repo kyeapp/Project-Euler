f1ab5246b6903c2742cecdcee6ff027774b963e4
paeynivek <paeynivek@gmail.com>
2014-05-21 08:59:43 -0700

    improved runtime.
    optimized check prime function. to check if any previously found primes
    are factors and up to sqrt(n)

--- problem010.cpp
+++ problem010.cpp
@@ -10,11 +10,14 @@ Find the sum of all the primes below two million.
 
 /*========== program design notes ==========
 We can use the check_prime function we wrote in problem 7.
-This method apparently takes a while.
+check_prime function written for problem 7 takes 7 minutes for this problem.
 
-proposed optimize solution: Store all the primes we find and just divide by all the primes 
-that we find and only if we go through the whole list then do we start dividing by numbers 
-incremented by 2 from our largest known prime.
+optimizing prime checking. When checking to see if n is prime we only need to check factors 
+up to the square root of n any numbers after that would have to be multiplied by another number under 
+sqrt(n) to get n. and not all numbers under n need to be divided. only the prime numbers under sqrt(n)
+need to be checked.
+
+time improve from 7 minutes to .5 seconds.
 =========================*/
 
 /*========== program output ==========
@@ -22,21 +25,23 @@ incremented by 2 from our largest known prime.
 
 answer: 142913828922
 =========================*/
-//current method takes about 7 minutes to find the answer.
 
+#include <cmath>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 long find_sum_primes(long limit);
-long check_prime(long num);
+long check_prime(vector<int>& primes, long num);
 
 int main() {
   
-  cout << "\t== Project Euler: Problem 10 ==" << endl << endl; 
-  cout << "\tWarning: this program takes 7 minutes to execute" << endl;
-  
   long limit = 2000000; //2 million
   long result = find_sum_primes(limit);
+
+
+
+  cout << "\t== Project Euler: Problem 10 ==" << endl << endl; 
   
   cout << "answer: " << result << endl << endl; 
   
@@ -47,21 +52,36 @@ int main() {
 long find_sum_primes(long limit) {
   long total = 2; //start at two to account for the first prime.
     
+  vector<int> primes; //vector to contain primes that are found.
+  primes.push_back(2); //push 2 into the vector as the first prime.
+    
   for (long i = 3; i < limit; i+=2) {
     //cout << i << endl;
-    total = total + check_prime(i);
+    total = total + check_prime(primes, i);
+    //cout << check_prime(primes, i) << endl; 
   }
  
   return total;
 }
 
 //returns num if it is prime. left out even number checks for speed.
-long check_prime(long num) {
-  long half = num / 2;
- 
-  for (long i = 3; i < half; i += 2) {
+long check_prime(vector<int>& primes, long num) {
+  long size = primes.size();
+  long root = sqrt(num) + 1;
+  long i = 0;
+  //cout << i << endl; 
+  
+  //check against all primes previously found
+  while (i < size && primes[i] < root) {
+    if ((num%primes[i]) == 0) { return 0; }
+    i++;
+  }
+  
+  //check up rest of odd numbers up to sqrt(num)
+  for (i = primes[size-1]; i < root; i += 2) {
     if ((num%i) == 0) { return 0; }
   }
   
+  primes.push_back(num);
   return num;
 }
