458d57addfc31f866e96079a5ad27ae8ee29dc0e
paeynivek <paeynivek@gmail.com>
2014-05-21 19:13:27 -0700

    finished 27 and 28

--- problem027.cpp
+++ problem027.cpp
@@ -23,27 +23,96 @@ maximum number of primes for consecutive values of n, starting with n = 0.
 =========================*/
 
 /*========== program design notes ==========
+two for loops to loop through all combinations of a and b
+once once loop for a, θ(n)
+once loop for b, θ(n)
+last loop to check for primes, θ(sqrt(n))
 
+This program shouldn't need a BigInt library
 =========================*/
 
 /*========== program output ===========
         == Project Euler: Problem 27 ==
 
-answer: 
+answer: -59231
 =========================*/
 
+#include <cmath>
 #include <iostream>
-#include "BigIntegerLibrary.hh"
 using namespace std;
 
-
+int find_quadratic_eq(int limit);
+int quadratic_chain(int a, int b);
+int quadratic (int a, int b, int n); 
+int is_prime(int num);
 
 int main() {
+  int limit = 1000;
   
-  int result = 0;
+  cout << "\t== Project Euler: Problem 27 ==" << endl << endl;
   
-  cout << "\t== Project Euler: Problem 27 ==" << endl << endl; 
+  int result = find_quadratic_eq(limit);
+  
   cout << "answer: " << result << endl << endl; 
   
   return 0;
+}
+
+//the a and b of a quadratic equation that yields the highest number of consecutive primes numbers
+int find_quadratic_eq(int limit) {
+  int lower_bound = -limit+1; //general definition of lower bound, scalable
+  int upper_bound = abs(limit-1); //general definition of higher bound, scalable
+
+  int prime = 0; //this variable holds the number of consecutive primes of the current equation
+  int longest_prime = 0; //this variable hold the longest number of consecutive primes.
+  int longest_a = 0;
+  int longest_b = 0;
+  
+  for (int a = lower_bound; a <= upper_bound; a++) { //loop through all a
+    for (int b = lower_bound; b <= upper_bound; b++) { //loop through all b
+      prime = quadratic_chain(a, b);
+      
+      if (prime > longest_prime) {
+        longest_prime = prime;
+        longest_a = a;
+        longest_b = b;
+      }
+    }
+  }
+  
+  cout << "\ta: " << longest_a << " b: " << longest_b << " length: " << longest_prime << endl << endl;
+  return longest_a * longest_b; 
+}
+
+//returns length of the chain of primes created by the quadratic equation
+int quadratic_chain(int a, int b) {
+  int n = 0; //reset n
+  int num = quadratic (a, b, n); //quadratic equation, with n as 0.
+  
+  while (is_prime(num) != 0) { //keep plugging in n until it finds a non-prime
+    n++;
+    num = quadratic (a, b, n); //computer next number
+  }
+  
+  return n;
+}
+
+//returns n^2 + a*n + b
+int quadratic (int a, int b, int n) {
+  return n*n + a*n + b;
+}
+
+//returns num if prime and positive, else return 0.
+int is_prime(int num) {
+  int root = sqrt(num) + 1;
+  int i = 0;
+  
+  if ((num%2 == 0 && num != 2) || (num < 0)) { return 0; } //2 is the only even number that is prime.
+  
+  //checks odd numbers up to sqrt(num)
+  for (i = 3; i < root; i += 2) {
+    if ((num%i) == 0) { return 0; }
+  }
+  
+  return num;
 }
---
+++ problem028.cpp
@@ -0,0 +1,61 @@
+//Kevin Yeap
+/*========== Project Euler: Problem 28 ==========
+Number spiral diagonals
+Problem 28
+
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+[21] 22  23  24 [25]
+ 20 [07] 08 [09] 10
+ 19  06 [01] 02  11
+ 18 [05] 04 [03] 12
+[17] 16  15  14 [13]
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+=========================*/
+
+/*========== program design notes ==========
+I have observed that starting from the center we only need to add 2 to the number to get the bottom-right, 
+add the 2 more for the bottom-left key, add two more for the top-left, and then add two more for the top right.
+the we add 4 four times for the next corners, then 6, then 8. we'd only need to add numbers in the right pattern
+to get the answer. This can be found in linear time θ(n).
+=========================*/
+
+/*========== program output ===========
+        == Project Euler: Problem 28 ==
+
+answer: 669171001
+=========================*/
+
+#include <iostream>
+using namespace std;
+
+int spiral_diagonal_sum(int side_length);
+
+int main() {
+  int side_length = 1001;
+  int result = spiral_diagonal_sum(side_length);
+  
+  cout << "\t== Project Euler: Problem 28 ==" << endl << endl; 
+  cout << "answer: " << result << endl << endl; 
+  
+  return 0;
+}
+
+int spiral_diagonal_sum(int side_length) {
+  int total = 1; //starts at 1 for the center.
+  int limit = side_length * side_length;
+  int diag = 1; //numbers that land on the diagonal
+
+  for (int i = 2; diag < limit; i+=2) {
+    for (int j = 0; j < 4; j++) {
+      diag += i;
+      total += diag;
+      //cout << total << endl; 
+    }
+  }
+  
+  return total;
+}
